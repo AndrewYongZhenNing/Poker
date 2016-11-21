bddba19a72e992d0f305887cce97ad8dac58899b
AndrewYongZhenNing <ayzn95@gmail.com>
2016-11-21 06:25:47 +0000

    Card and Deck Class working
    Now about to work on a Game class and other necessary classes

---
+++ hilo.cpp
@@ -0,0 +1,82 @@
+#include "hilo.h"
+
+Card::Card(char Suit, int value){
+
+  _suit = Suit;
+  _value = value;
+
+}
+
+Card::Card(): _suit('a'),_value(0){};
+
+Card::~Card(){};
+
+char Card::getSuit(){
+
+  // std::cout << "The suit is: " << _suit << std::endl;
+  // std::cout << _suit << std::endl;
+
+  return _suit;
+
+}
+
+int Card::getValue(){
+
+  // std::cout << "The value is: " << _value << std::endl;
+  // std::cout << _value << std::endl;
+
+  return _value;
+}
+
+Deck::Deck(){
+
+  char _suit[4] = {'H', 'C', 'S', 'D'}; // defines the synbols for each suit using char
+
+  for (int i = 0; i < 4; i++){ // for every suit...
+
+    for (int j = 1; j < 14; j++){ //... give it twelve cardsd (Card object)
+
+      Card card(_suit[i],j);
+
+      /* THIS IS JUST FOR CHECKING THAT THE DECK HAS ALL THE VALUES FOR EVERY SUIT
+
+      // card.getSuit();
+      // card.getValue();
+
+      // std::cout << card.getSuit() << card.getValue() << std::endl;
+      // std::cout << card.getSuit() << std::endl;
+
+      */
+
+      _deck.push_back(card);
+
+    }
+  }
+
+
+}
+
+Deck::~Deck(){};
+
+void Deck::shuffle(){ // randomly shuffles the deck using srand in main
+  std::random_shuffle(_deck.begin(),_deck.end());
+}
+
+void Deck::show(){ //tested it on main, shows entire deck successfully
+
+  for (_deck_iter = _deck.begin(); _deck_iter != _deck.end(); _deck_iter++){
+    Card temp = *_deck_iter;
+    std::cout << temp.getSuit() << temp.getValue() << std::endl;
+  }
+
+  // std::cout << "does nothing" << std::endl;
+
+}
+
+void Deck::new_top(){ // takes the current top card and pushes it back to the bottom of the deck [a tradition, apparently]
+  _deck_iter = _deck.begin(); // points the pointer at the top most element
+
+  _deck.push_back(*_deck_iter); //pushes a value equivalent the Card object iter is pointing at (the top card) to the bottom of the deck
+  _deck.erase(_deck_iter); // now erase top Card object to avoid duplication
+  
+}
---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "hilo.h"
+#include <iostream>
+#include <ctime>
+
+int main(){
+
+  srand(time(NULL));
+
+  Deck mydeck = Deck();
+
+  std::cout << "Here is my deck:" << std::endl;
+
+  mydeck.show();
+
+  std::cout << "\nShuffling now...\n" << std::endl;
+
+  mydeck.shuffle(); // shuffle dem Card objects
+  mydeck.show(); // show all the Card objects
+
+  std::cout << "\nPopping top card to the bottom...\n" << std::endl;
+
+  mydeck.new_top();
+  mydeck.show();
+
+
+
+
+}
