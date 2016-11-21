c517eee42bb0ae3ba7d22818c338665c9555a17a
AndrewYongZhenNing <ayzn95@gmail.com>
2016-11-21 12:50:33 +0000

    flop turn river complete

--- poker.h
+++ poker.h
@@ -80,17 +80,20 @@ public:
 
   Game();
   ~Game();
-  void show_players();
+  void show_players(); // made for testing purporses
   void deal_pocket(Deck &deck); // deal pocket cards to each player
-  // void deal_flop(); // deals first 3 community cards
-  // void deal_turn(); // deals 4th community card
-  // void deal_river(); // deals 5th community card N.B essentially deal_turn and deal_river have some methods, but I made different functions as to avoid any unforseen subtle errors, can change later
+  void deal_flop(Deck &deck); // deals first 3 community cards
+  void deal_turn(Deck &deck); // deals 4th community card
+  void deal_river(Deck &deck); // deals 5th community card
+  //N.B essentially deal_turn and deal_river have some methods, but I made different functions as to avoid any unforseen subtle errors, can change later
+  void show_board();
 
 protected:
   int _number_of_players;
   std::vector<Player> _players; // a vector container to contain all Player object profiles
   std::vector<Player>::iterator _players_iter;
   std::vector<Card> _board; // this is where the community Card objects are placed
+  std::vector<Card>::iterator _board_iter; // for checking if _board has at least 3 cards after flop
 
 };
 
